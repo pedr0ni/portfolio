16361b05920b8a3fd6f69eb607c343e230b7ce76
Matheus Pedroni <pedroni.dev@gmail.com>
2019-07-16 00:55:56 -0300

    ✨ built dist

---
+++ dist/precache-manifest.80d57ad51b79619cc574630e5158a62e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "76102893d11c451fef846b6a82fa4dd8",
+    "url": "/img/facebook.76102893.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e7c2294a70c75cf8f97b",
+    "url": "/js/chunk-vendors.98b5c160.js"
+  },
+  {
+    "revision": "d4e0249a7bab806892a1",
+    "url": "/js/app.0130f28e.js"
+  },
+  {
+    "revision": "abd5a7fd0d30b918cfeddfe3d6e0ae8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1b904cad3f25d874b2aa1070a3bc26",
+    "url": "/img/twitter.de1b904c.png"
+  },
+  {
+    "revision": "7f163d2b3f92f30654b6f2fb6b200682",
+    "url": "/img/profile.7f163d2b.jpg"
+  },
+  {
+    "revision": "d1d54f80d17b5cd80f1cd1482d7fa3db",
+    "url": "/img/linkedin.d1d54f80.png"
+  },
+  {
+    "revision": "1911c85b868e0a0d2f974170a3d3d999",
+    "url": "/img/instagram.1911c85b.png"
+  },
+  {
+    "revision": "f9e15284367ca515c7f1cc146e5d831e",
+    "url": "/img/github.f9e15284.png"
+  },
+  {
+    "revision": "d4e0249a7bab806892a1",
+    "url": "/css/app.14757d83.css"
+  }
+];
